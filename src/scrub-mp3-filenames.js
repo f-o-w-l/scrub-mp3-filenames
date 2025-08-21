0ae4c98e44865bd38c5090f0f719b126c6b959a8
f-o-w-l <olfowler@pm.me>
2025-08-21 16:41:45 -0500

    chore: move script to src folder

--- src/scrub-mp3-filenames.js
+++ src/scrub-mp3-filenames.js
@@ -2,10 +2,13 @@ import fs from 'node:fs'
 import path from 'node:path'
 import NodeID3 from 'node-id3'
 
+/**
+ * Look in root project dir, no need for subfolder, only .mp3 files scrubbed
+ */
 const folder = '.'
 const files = fs.readdirSync(folder).filter((f) => f.endsWith('.mp3'))
 
-const removeCrap = (str) => {
+const scrubString = (str) => {
   return str
     .replace(/(?:\s*-\s*)?\[?SPOTIFY-DOWNLOADER\.COM\]?/i, '')
     .replace(/(?:\s*-\s*)?\[?OneFireMusic\.Net\]?/i, '')
@@ -21,16 +24,16 @@ for (const file of files) {
   const fullPath = path.join(folder, file)
   const tags = NodeID3.read(fullPath)
 
-  const fileName = path.parse(file).name
-  const fixedFileName = removeCrap(fileName)
+  const filename = path.parse(file).name
+  const scrubbedFilename = scrubString(filename)
 
   let trackNum = tags.trackNumber
     ? parseInt(tags.trackNumber.split('/')[0], 10)
     : null
 
-  // Attempt to get track number from fileName
+  // Attempt to get track num from filename if undefined in metadata
   if (!trackNum) {
-    const trackNumInFileNameMatch = fileName.match(/^(\d+)([.\-\s])/)
+    const trackNumInFileNameMatch = filename.match(/^(\d+)([.\-\s])/)
     if (trackNumInFileNameMatch) {
       trackNum = parseInt(trackNumInFileNameMatch[1], 10)
     }
@@ -39,12 +42,12 @@ for (const file of files) {
   if (!trackNum) throw new Error(`Track number not found for file: ${file}`)
 
   const paddedTrackNum = trackNum.toString().padStart(2, '0')
-  const newFileName = `${paddedTrackNum}. ${fixedFileName}.mp3`
-  const newPath = path.join(folder, newFileName)
+  const newFilename = `${paddedTrackNum}. ${scrubbedFilename}.mp3`
+  const newPath = path.join(folder, newFilename)
 
-  // Update MP3 title metadata
-  const curTitle = tags.title || fileName
-  const newTitle = removeCrap(curTitle)
+  // Update MP3 metadata
+  const curTitle = tags.title || filename
+  const newTitle = scrubString(curTitle)
 
   if (curTitle !== newTitle) {
     NodeID3.update(
@@ -59,11 +62,11 @@ for (const file of files) {
     console.warn(`[SKIP TITLE]:    "${curTitle}"`)
   }
 
-  // Update file name
-  if (newFileName !== fileName) {
+  // Update MP3 filename
+  if (newFilename !== filename) {
     fs.renameSync(fullPath, newPath)
-    console.log(`Renamed file:    "${file}" -> "${newFileName}"`)
+    console.log(`Renamed file:    "${file}" -> "${newFilename}"`)
   } else {
-    console.warn(`[SKIP FILENAME]: "${fileName}"`)
+    console.warn(`[SKIP FILENAME]: "${filename}"`)
   }
 }
